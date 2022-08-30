51e621054db090fdc96a16f73ff42cb6712f8a95
nunya <nunya@business.com>
2022-08-29 23:48:52 -0500

    main better

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,8 @@
 mod vectors;
 mod util;
-
-use crate::vectors::{
-    Quaternion,
-    Vec3
-};
+mod traits;
 
 
-struct Box {
-    position: Quaternion,
-    size: Vec3
-}
 
 fn main() {
     println!("Hello, world!");
